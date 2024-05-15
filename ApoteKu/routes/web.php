a836ec9d6ce81d341ff2b55ce7528a5db7969f4f
Ferry Aditya H <ferryaditya124@gmail.com>
2024-05-15 21:38:16 +0700

    #Pembuatan View Dashboard, Login, Beranda. Melakukan Layouting Dashboard agar mudah untuk kedepannya

--- ApoteKu/routes/web.php
+++ ApoteKu/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//beranda
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function(){
+    return view('Authentication.login');
+});
+
+Route::get('/dashboard', function(){
+    return view('Admin.dashboard');
 });
